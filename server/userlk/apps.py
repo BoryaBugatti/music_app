f26158bd8a49223c626cadda2c435063a3e0e7cb
BoryaBugatti <bl4856@gmail.com>
2025-09-30 00:59:14 +0300

    Delete server/userlk directory

--- server/userlk/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserlkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userlk'
--- server/userlk/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from userlk.models import User
-from userlk.serializers import userSerializer
-# Create your views here.
-class GetAllUsers(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = userSerializer
